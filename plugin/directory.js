da0bd0799b57549fa2af09892ef264375cb32e9b
Yoshihisa Sakuraba <ys80276@partner.technosystems.io>
2023-06-28 14:26:09 +0900

    first

--- plugin/directory.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (fastify, options, done) {
-  fastify.get("/hello", (request, reply) => {
-    reply.send({ message: "Hello from the plugin!" });
-  });
-
-  done(); // プラグインの登録完了を示す
-};
