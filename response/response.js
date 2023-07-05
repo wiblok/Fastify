9b700a956eced0b628d53045ab27277eecb42b0d
wiblok <asakura.teiroku@gmail.com>
2023-07-05 22:55:40 +0900

    a

--- response/response.js
+++ response/response.js
@@ -1,21 +1,36 @@
 const fastify = require("fastify")();
 
-fastify.get("/", async (request, reply) => {
-  // JSONレスポンスの生成
+// JSONレスポンスの生成
+fastify.get("/json", async (request, reply) => {
   const jsonResponse = {
     message: "This is a JSON response",
   };
   reply.send(jsonResponse);
-  // HTMLレスポンスの生成
+});
+
+// HTMLレスポンスの生成
+fastify.get("/html", async (request, reply) => {
   const htmlResponse = "<h1>This is an HTML response</h1>";
   reply.type("text/html").send(htmlResponse);
-  // レスポンスヘッダーの設定
-  reply.header("Custom-Header", "Custom Value");
-  // ステータスコードの設定
+});
+
+// レスポンスヘッダーの設定
+fastify.get("/header", async (request, reply) => {
+  reply.header("Custom-Header", "Custom Value").send("Header set");
+});
+
+// ステータスコードの設定
+fastify.get("/status", async (request, reply) => {
   reply.status(201).send("Created");
-  // リダイレクトの実行
+});
+
+// リダイレクトの実行
+fastify.get("/redirect", async (request, reply) => {
   reply.redirect("/new-location");
-  // エラーレスポンスの生成
+});
+
+// エラーレスポンスの生成
+fastify.get("/error", async (request, reply) => {
   const errorResponse = {
     error: "An error occurred",
   };
